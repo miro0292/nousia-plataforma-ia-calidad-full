438c766fd7e5cbdc1aca702ea294c78636ab0050
Miguel <miro0292@gmail.com>
2025-11-28 20:09:16 -0500

    fix: update script source path in index.html and ensure consistent base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nousia-plataforma-ia-calidad-full/' 
+  base: '/nousia-plataforma-ia-calidad-full/'
 })
